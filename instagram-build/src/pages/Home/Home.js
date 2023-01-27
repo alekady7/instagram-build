1ddffc7ff9e0ae66e4ff789feb44318f76da4881
Aditya Uniyal <47002387+alekady7@users.noreply.github.com>
2023-01-27 19:09:11 +0530

    instagram-build

---
+++ instagram-build/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from '../../screens/Login/Login'
+
+
+function Home() {
+    return (
+        <div className='home'>
+            <Login />
+            {/* <Navbar /> */}
+        </div>
+    )
+}
+
+export default Home
---
+++ instagram-build/src/screens/Login/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Login.css"
+
+const Login = () => {
+    return (
+        <div className="cover">
+            <h1>Login</h1>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+
+            <div className="login-btn">Login</div>
+        </div>
+    )
+}
+
+export default Login
